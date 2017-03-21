bb0d4f5d6de27dc9d5190d5586f0c017062d39a0
Nikolina Shopova <nikolina.shopova@gmail.com>
2017-03-21 18:37:05 +0100

    course visialisation complete

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,4 +2,19 @@ $(function() {
 
   // your code will go here
 
+
+    $.ajax({
+    url: 'https://www.codeschool.com/users/1651036.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      var completedCourses = response.courses.completed;
+      for(var i=0; i< completedCourses.length; i++){
+        $("#badges").append('<div class="course"><h3>'+completedCourses[i].title+
+        '</h3><img src="'+ completedCourses[i].badge+'"/><a href= "'+ completedCourses[i].url + 
+        '"target="_blank" class="btn btn-primary">See Course</a></div>');  
+      }
+    }
+  });
+
 });
